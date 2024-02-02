2a2b2baed653ff49919e52847bca287a275c01ae
LeylaBackend <leylaunusmuhamedova@gmail.com>
2024-02-02 12:56:30 +0600

    hw5

--- Afisha/urls.py
+++ Afisha/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('movie_app.urls'))
+    path('api/v1/', include('movie_app.urls')),
+    path('api/v1/users/', include('users.urls')),
 ]
