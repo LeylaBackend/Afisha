1f4862e0bf5d62d18a67a330b1cd7a1102851248
LeylaBackend <leylaunusmuhamedova@gmail.com>
2024-02-05 22:10:36 +0600

    hw6

--- movie_app/urls.py
+++ movie_app/urls.py
@@ -2,12 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('directors/', views.director_list_api_view),
-    path('directors/<int:id>/', views.director_detail_api_view),
-    path('movies/', views.movie_list_api_view),
-    path('movies/<int:id>/', views.movie_detail_api_view),
-    path('review/', views.review_list_api_view),
-    path('review/<int:id>/', views.review_detail_api_view),
+    # path('directors/', views.director_list_api_view),
+    # path('directors/<int:id>/', views.director_detail_api_view),
+    path('directors/', views.DirectorListCreateAPIView.as_view()),
+    path('directors/<int:id>/', views.DirectorDetailAPIView.as_view()),
+    # path('movies/', views.movie_list_api_view),
+    # path('movies/<int:id>/', views.movie_detail_api_view),
+    path('movies/', views.MovieListCreateAPIView.as_view()),
+    path('movies/<int:id>/', views.MovieDetailAPIView.as_view()),
+    # path('review/', views.review_list_api_view),
+    # path('review/<int:id>/', views.review_detail_api_view),
+    path('review/', views.ReviewListCreateAPIView.as_view()),
+    path('review/<int:id>/', views.ReviewDetailAPIView.as_view()),
     path('movies/reviews', views.movie_review_list_api_view),
 
 ]
--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.registration_user_api_view),
-    path('login/', views.login_user_api_view),
-    path('confirm/', views.confirm_user_api_view),
+    # path('register/', views.registration_user_api_view),
+    # path('login/', views.login_user_api_view),
+    # path('confirm/', views.confirm_user_api_view),
+    path('register/', views.UserRegistrationView.as_view()),
+    path('login/', views.UserLoginView.as_view()),
+    path('confirm/', views.ConfirmUserView.as_view()),
     ]
